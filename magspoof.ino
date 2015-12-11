69648503ca8f257743d88681f44c5e0669c8a141
miaoski <lin.zhemin@gmail.com>
2015-12-11 16:31:39 +0800

    Need some delay inbetween.  % still not ready

--- magspoof.ino
+++ magspoof.ino
@@ -3,12 +3,8 @@
 // and port something to Arduino Nano
 // - miaoski
 
-void writeLow();
 void setPolarity(int polarity);
 void playBit(int bit);
-unsigned char *getEncoding(unsigned char character);
-void writeEncodedCharacter(char character);
-void writeSequence(int len, unsigned char sequence[]);
 
 #define PIN_A    3      // left pin, to L293D pin 2
 #define PIN_B    4      // right pin, to L293D pin 7
@@ -16,18 +12,17 @@ void writeSequence(int len, unsigned char sequence[]);
 #define BUTTON_PIN 12
 #define CLOCK_US 200
 #define BETWEEN_ZERO 53 // 53 zeros between track1 & 2
-#define TRACKS 2
 
 int polarity = 0;
 unsigned int curTrack = 0;
-char revTrack[41];
+char revTrack[80];
 
 const int sublen[] = { 32, 48, 48 };
 const int bitlen[] = {  7,  5,  5 };
 
 const char* tracks[] = {
-"%B123456781234567^LASTNAME/FIRST^YYMMSSSDDDDDDDDDDDDDDDDDDDDDDDDD?\0", // Track 1
-";123456781234567=YYMMSSSDDDDDDDDDDDDDD?\0" // Track 2
+"%B4503111122223333^MIAOSKI LIN              ^2010200999909999999900990000000?",	// Track 1; %BPAN^NAME^YYMMSSSCVV? + LRC
+";4503111122223333=200120099999999?" // Track 2 ;PAN=YYMMSSSDIS? + LRC
 };
 
 void setup() {
@@ -36,12 +31,6 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   Serial.begin(9600);
   storeRevTrack(2);
-}
-
-// Set the voltage to 0
-void writeLow() {
-  digitalWrite(PIN_A, LOW);
-  digitalWrite(PIN_B, LOW);
 }
 
 // Set the polarity of the elctromagnet.
@@ -74,9 +63,9 @@ void reverseTrack(int track) {
   track--; // index 0
   polarity = 0;
 
-  while (revTrack[i++] != '\0');
+  while(revTrack[i++] != '\0');
   i--;
-  while (i--)
+  while(i--)
     for (int j = bitlen[track]-1; j >= 0; j--)
       playBit((revTrack[i] >> j) & 1);
 }
@@ -92,31 +81,29 @@ void playTrack(int track)
   digitalWrite(ENABLE_PIN, HIGH);
 
   // First put out a bunch of leading zeros.
-  for (int i = 0; i < 25; i++)
+  for(int i = 0; i < 25; i++)
     playBit(0);
 
-  //
-  Serial.println(tracks[track]);
-  for (int i = 0; tracks[track][i] != '\0'; i++)
-  {
+  Serial.print("Sending: ");
+  for(int i = 0; tracks[track][i] != '\0'; i++) {
+    Serial.print(tracks[track][i]);
     crc = 1;
     tmp = tracks[track][i] - sublen[track];
 
-    for (int j = 0; j < bitlen[track]-1; j++)
-    {
-      crc ^= tmp & 1;
+    for(int j = 0; j < bitlen[track]-1; j++) {
+      crc ^= (tmp & 1);
       lrc ^= (tmp & 1) << j;
       playBit(tmp & 1);
       tmp >>= 1;
     }
     playBit(crc);
-  } 
+  }
+  Serial.println();
 
   // finish calculating and send last "byte" (LRC)
   tmp = lrc;
   crc = 1;
-  for (int j = 0; j < bitlen[track]-1; j++)
-  {
+  for(int j = 0; j < bitlen[track]-1; j++) {
     crc ^= tmp & 1;
     playBit(tmp & 1);
     tmp >>= 1;
@@ -124,25 +111,20 @@ void playTrack(int track)
   playBit(crc);
 
   // if track 1, play 2nd track in reverse (like swiping back?)
-  if (track == 0)
-  {
-    // if track 1, also play track 2 in reverse
-    // zeros in between
-    for (int i = 0; i < BETWEEN_ZERO; i++)
+/*
+  if(track == 0) {
+    for(int i = 0; i < BETWEEN_ZERO; i++)
       playBit(0);
-
-    // send second track in reverse
     reverseTrack(2);
   }
-
+  */
   // finish with 0's
-  for (int i = 0; i < 5 * 5; i++)
+  for (int i = 0; i < 25; i++)
     playBit(0);
 
   digitalWrite(PIN_A, LOW);
   digitalWrite(PIN_B, LOW);
   digitalWrite(ENABLE_PIN, LOW);
-
 }
 
 
@@ -190,11 +172,13 @@ void storeRevTrack(int track) {
 
  
 void loop() {
+  memset(revTrack, 1, sizeof(revTrack));
   while(digitalRead(BUTTON_PIN) == HIGH);
 
   delay(50);
   while(digitalRead(BUTTON_PIN) == HIGH);
-  Serial.println("Sending");
+  Serial.print("Track ");
+  Serial.println(1 + (curTrack % 2));
   playTrack(1 + (curTrack++ % 2)); 
   delay(400);
 }
